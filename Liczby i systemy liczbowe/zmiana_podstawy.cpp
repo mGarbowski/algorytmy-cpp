41004f00a34d6b5145f6dd5b740bf55bc25eff86
mGarbowski <mikolaj.garbowski@gmail.com>
2021-03-21 11:54:36 +0100

    zmiana podstawy

---
+++ Liczby i systemy liczbowe/zmiana_podstawy.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string zmienPodstawe(int liczba, int podstawa)
+{
+    // Zwraca string będący reprezentacją liczby dla podanej podstawy
+    // Maksymalna podstawa - 16
+    
+    if (podstawa > 16)
+        return "Niepoprawna podstawa";
+
+    string cyfry = "0123456789ABCDEF";
+    string wynik = "";
+
+    while (liczba > 0)
+    {
+        wynik = cyfry[liczba % podstawa] + wynik;
+        liczba = liczba / podstawa;
+    }
+
+    return wynik;
+}
+
+int oblicz(string liczba, int podstawa)
+{
+    // Zwraca wartość liczby w danej podstawie (w systemie dziesietnym)
+    // Maksymalna podstawa - 16
+
+    if (podstawa > 16)
+        return -1;
+
+    int wynik = 0;
+    int potega = 1;
+
+    for (int i = liczba.size()-1; i >= 0; i--)
+    {
+        if (liczba[i] >= 'A' && liczba[i] <= 'F')
+        {
+            wynik += (liczba[i] - 'A' + 10) * potega;
+        }
+        if (liczba[i] >= '0' && liczba[i] <= '9')
+        {
+            wynik += (liczba[i] - '0') * potega;
+        }
+
+        potega = potega * podstawa;
+    }
+
+    return wynik;
+}
+
+int main()
+{
+    string liczba; 
+    int podstawa;
+    
+    cout << "Liczba ";
+    cin >> liczba;
+    cout << "Podstawa ";
+    cin >> podstawa;
+
+    cout << oblicz(liczba, podstawa);
+    
+    return 0;
+}
