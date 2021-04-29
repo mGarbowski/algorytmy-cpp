17e76580e0fa9b6a31813f4cd7ac6e6034442ad7
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2021-04-29 12:43:21 +0200

    miejsca zerowe

---
+++ Float/miejsca_zerowe.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Porownywanie z wartoscia epsilon za miast z 0 dla unikniecia bledow numerycznych
+const float EPS = 0.000001;
+
+float f(float x)
+{
+    return x*x*x - 6*x*x + 3*x + 10;
+}
+
+
+int main()
+{
+    // Szukanie miejsca zerowego w danym przedziale metoda bisekcji (wyszukiwanie binarne)
+
+    float low, mid, high;
+
+    cout << "Dolna granica przedzialu = ";
+    cin >> low;
+    cout << "Gorna granica przedzialu = ";
+    cin >> high;
+
+
+    if (f(low) * f(high) < 0)
+    {
+        mid = (low + high) / 2;
+
+        while ((abs(f(mid)) > EPS) && (high-low > EPS))
+        {
+            if (f(low) * f(mid) < 0)
+            {
+                high = mid;
+            }
+            else 
+            {
+                low = mid;
+            }
+
+            mid = (low + high) / 2;
+        }
+
+        cout << "Miejsce zerowe: " << mid;
+    }
+    else 
+    {
+        cout << "Nieprawidlowy przedzial";
+    }
+
+    return 0;
+}
